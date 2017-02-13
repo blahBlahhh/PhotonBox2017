d7f67dd7327070d70e2bcc714a35d60ea1ed4dda
blahBlahhh <1205895614@qq.com>
2017-02-13 21:25:28 +0800

    AutoGearBlueRight added (need tests)

---
+++ src/org/usfirst/frc/team6179/robot/commands/AutoGearBlueRight.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team6179.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutoGearBlueRight extends CommandGroup
+{
+	public AutoGearBlueRight()
+	{
+		addSequential(new DriveForward(-52.6, 1));   //52.6cm =====>133.6inches
+		addSequential(new DriveSwervely(-60, 1));    //Counterclockwise 60 degrees
+		addSequential(new DriveForward(-8.8, 1));    //8.82cm======>22.4inches
+		addSequential(new DeliverGear());
+		Timer.delay(2); 							//TODO Revise after testing
+		addSequential(new DriveForward(8.8,1));
+	}
+}
